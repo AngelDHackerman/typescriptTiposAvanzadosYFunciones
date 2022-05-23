769320b8bd43c890696b612bbd2c2d09c8da10d4
angelDariaux <angeldariaux@gmail.com>
2022-05-22 19:39:49 -0600

    usando los rest params ...

--- src/07restParams.ts
+++ src/07restParams.ts
@@ -39,11 +39,19 @@ export const checkRoleV2 = (roles: string[]) => {
   return false;
 }
 
-const rta3 = checkRoleV2([ROLES.ADMIN, ROLES.SELLER]);
-console.log('checkRole:', rta3);
+const rta3 = checkRoleV2([ROLES.ADMIN, ROLES.SELLER]);    // * pasamos los parametros dentro de un array.
+console.log('checkRoleV2:', rta3);
 
 
 
-      // ? Rest params
+      // ? Rest params, usamos el spread operator "..." y ya NO pasamos los parametros de la funcion como un array
 
+export const checkRoleV3 = (...roles: string[]) => { // ! usando el ... para ya NO pasar los parametros dentro de un array
+  if (roles.includes(currentUser.role)) {
+    return true;
+  }
+  return false;
+}
 
+const rta4 = checkRoleV3 (ROLES.ADMIN, ROLES.CUSTOMER, ROLES.SELLER); // todo: Ya NO pasamos los paramentros dentro de un array, TS lo hace automaticamente por nosotros
+console.log('checkRoleV3', rta4);
