a6130821200daaa179eae70660962ebe6ae4188b
angelDariaux <angeldariaux@gmail.com>
2022-05-23 18:47:28 -0600

    usando el readonly para proteger los valores de una interface

--- src/app/base.model.ts
+++ src/app/base.model.ts
@@ -1,5 +1,5 @@
 export interface BaseModel {
-  id: string | number;
-  createdAt: Date;
-  updatedAt: Date;
+  readonly id: string | number; // ! readonly, nos proteje este valor para que NO sea sobrescrito.
+  readonly createdAt: Date;
+  readonly updatedAt: Date;
 }
