6ca5fe2bf78daccb039588c3368208716ddc10d0
angelDariaux <angeldariaux@gmail.com>
2022-05-23 19:15:47 -0600

    agregada la libreria de faker

--- src/app/products/product.model.ts
+++ src/app/products/product.model.ts
@@ -6,8 +6,14 @@ import { Category } from "../categories/category.model";
 export type Sizes = 'S' | 'M' | 'L' | 'XL';
 export interface Product extends BaseModel {    // extends BaseModel, esto le va agregar todas las propiedades de BaseModel al Product model. (Esto es la herencia)
   title: string;
+  image: string;
+  description: string;
   stock: number;
-  size?: Sizes;
+  color: string;
+  price: number
   category: Category; // En lugar de hacer un objeto anidado creamos un modulo en category.
+  isNew: boolean;
+  tags: string[];
+  size?: Sizes;
 }
 
