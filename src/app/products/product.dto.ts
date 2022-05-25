2a0da2ce036c339812aa8b2dc3a58337e5d2d072
angelDariaux <angeldariaux@gmail.com>
2022-05-24 19:16:58 -0600

    usando partial y required

--- src/app/products/product.dto.ts
+++ src/app/products/product.dto.ts
@@ -1,16 +1,21 @@
 import { Product } from '../products/product.model'
 
-// ? Omit, omite los campos del DTO (data transfer object).
-// ? Pick, es el contrario de Omit, este elige que valores vamos a transferir.
-// ? Esto avita que se genere el 'id', 'createdAt', etc...
-
+// ? Omit, omite los campos del DTO (data transfer object). Esto avita que se genere el 'id', 'createdAt', etc...
 export interface CreateProductDto extends Omit<Product, 'id' | 'createdAt' | 'updatedAt' | 'category'> {
   categoryId: string;
 }
 
+// ? Pick, es el contrario de Omit, este elige que valores vamos a transferir.
+
 type example = Pick<Product, 'color' | 'description'>;
 
-// ! Partial, pone todos opciones de tipado como opcionales "name?: 'juanito'"
+
+
+// ? Partial, pone todos opciones de tipado como opcionales "name?: 'juanito'"
 export interface UpdateProductDto extends Partial<CreateProductDto> {
 
 }
+
+// ? Required, hace que todas las opciones de tipado sean REQUERIDAS
+
+type example2 = Required<Product>;
