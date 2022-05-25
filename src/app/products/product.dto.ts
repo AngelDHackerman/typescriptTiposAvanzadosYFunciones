9dfe850a92c712c631da9d5f43afa1f696301c12
angelDariaux <angeldariaux@gmail.com>
2022-05-24 18:54:10 -0600

    usando pick y ommit en los DTO data transfer object

--- src/app/products/product.dto.ts
+++ src/app/products/product.dto.ts
@@ -1,7 +1,11 @@
 import { Product } from '../products/product.model'
 
+// ? Omit, omite los campos del DTO (data transfer object).
+// ? Pick, es el contrario de Omit, este elige que valores vamos a transferir.
 // ? Esto avita que se genere el 'id', 'createdAt', etc...
 
 export interface CreateProductDto extends Omit<Product, 'id' | 'createdAt' | 'updatedAt' | 'category'> {
   categoryId: string;
 }
+
+type example = Pick<Product, 'color' | 'description'>;
